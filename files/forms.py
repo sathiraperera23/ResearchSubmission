dccefeeed9857d12ffbe97b79be6c2ded6997521
Sathira Perera <sathira.perera23@gmail.com>
2022-09-30 15:54:24 +0530

    Added files to model and form

--- files/forms.py
+++ files/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 
+from files.models import FileData
+
+
 class StudentForm(forms.Form):
     firstname = forms.CharField(label="Enter first name", max_length=50)
     lastname = forms.CharField(label="Enter last name", max_length=10)
     email = forms.EmailField(label="Enter Email")
     file = forms.FileField()  # for creating file input
+
+
+class FileForm(forms.Form):
+    class Meta:
+        model = FileData
+        fields = ["firstname", "lastname", "email", "file"]
--- files/models.py
+++ files/models.py
@@ -1,5 +1,16 @@
+from typing_extensions import Required
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+
 class Files(models.Model):
-   file = models.FileField(upload_to='documents/'),
+    file = models.FileField(upload_to='documents/'),
+
+
+class FileData(models.Model):
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=20)
+    email = models.EmailField()
+    file = models.FileField(Required)
